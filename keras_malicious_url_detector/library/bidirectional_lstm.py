9edb06c29ce22d3690f215d866bbcf60234bd2a5
xschen <xs0040@gmail.com>
2018-02-24 22:35:46 +0800

    refactor the codes in malicious url detector

---
+++ keras_malicious_url_detector/library/bidirectional_lstm.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+from malicious_url_train.bidirectional_lstm_train import make_bidirectional_lstm_model
+from malicious_url_train.url_data_loader import load_url_data
+
+
+class BidirectionalLstmEmbedPredictor(object):
+    model_name = 'bidirectional-lstm'
+
+    def __init__(self):
+        self.model = None
+        self.num_input_tokens = None
+        self.idx2char = None
+        self.char2idx = None
+        self.max_url_seq_length = None
+
+    @staticmethod
+    def get_config_file_path(model_dir_path):
+        return model_dir_path + '/' + BidirectionalLstmEmbedPredictor.model_name + '-config.npy'
+
+    @staticmethod
+    def get_weight_file_path(model_dir_path):
+        return model_dir_path + '/' + BidirectionalLstmEmbedPredictor.model_name + '-weights.h5'
+
+    def load_model(self, model_dir_path):
+        config_file_path = self.get_config_file_path(model_dir_path)
+        weight_file_path = self.get_weight_file_path(model_dir_path)
+
+        config = np.load(config_file_path).item()
+        self.num_input_tokens = config['num_input_tokens']
+        self.max_url_seq_length = config['max_url_seq_length']
+        self.idx2char = config['idx2char']
+        self.char2idx = config['char2idx']
+
+        self.model = make_bidirectional_lstm_model(self.num_input_tokens, self.max_url_seq_length)
+        self.model.load_weights(weight_file_path)
+
+    def predict(self, url):
+        data_size = 1
+        X = np.zeros(shape=(data_size, self.max_url_seq_length))
+        for idx, c in enumerate(url):
+            if c in self.char2idx:
+                X[0, idx] = self.char2idx[c]
+        predicted = self.model.predict(X)[0]
+        predicted_label = np.argmax(predicted)
+        return predicted_label
+
+
+
---
+++ keras_malicious_url_detector/library/lstm.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from malicious_url_train.lstm_train import make_lstm_model
+from malicious_url_train.url_data_loader import load_url_data
+
+
+class LstmPredictor(object):
+
+    model = None
+    num_input_tokens = None
+    idx2char = None
+    char2idx = None
+    max_url_seq_length = None
+
+    def __init__(self):
+        pass
+
+    def load_model(self, config_file_path, weight_file_path):
+        config = np.load(config_file_path).item()
+        self.num_input_tokens = config['num_input_tokens']
+        self.max_url_seq_length = config['max_url_seq_length']
+        self.idx2char = config['idx2char']
+        self.char2idx = config['char2idx']
+
+        self.model = make_lstm_model(self.num_input_tokens)
+        self.model.load_weights(weight_file_path)
+
+    def predict(self, url):
+        data_size = 1
+        X = np.zeros(shape=(data_size, self.max_url_seq_length, self.num_input_tokens))
+        for idx, c in enumerate(url):
+            if c in self.char2idx:
+                X[0, idx, self.char2idx[c]] = 1
+        predicted = self.model.predict(X)[0]
+        predicted_label = np.argmax(predicted)
+        return predicted_label
+
+
+def main():
+    model_name = 'lstm'
+    data_dir_path = '../malicious_url_train/data'
+    model_dir_path = '../malicious_url_train/models'
+    config_file_path = model_dir_path + '/' + model_name + '-config.npy'
+    weight_file_path = model_dir_path + '/' + model_name + '-weights.h5'
+    predictor = LstmPredictor()
+    predictor.load_model(config_file_path=config_file_path, weight_file_path=weight_file_path)
+
+    url_data = load_url_data(data_dir_path)
+    count = 0
+    for url, label in zip(url_data['text'], url_data['label']):
+        predicted_label = predictor.predict(url)
+        print('predicted: ' + str(predicted_label) + ' actual: ' + str(label))
+        count += 1
+        if count > 20:
+            break
+
+
+if __name__ == '__main__':
+    main()
