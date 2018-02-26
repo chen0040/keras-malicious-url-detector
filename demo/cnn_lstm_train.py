d2abfa6810e5caf10e7d02c4d0eeeb0c6a6df565
xschen <xs0040@gmail.com>
2018-02-26 10:16:36 +0800

    train script for cnn lstm

---
+++ demo/cnn_lstm_train.py
@@ -0,0 +1,35 @@
+from keras_malicious_url_detector.library.cnn_lstm import CnnLstmPredictor
+from malicious_url_train.url_data_loader import load_url_data
+import numpy as np
+from keras_malicious_url_detector.library.utility.text_model_extractor import extract_text_model
+from malicious_url_train.utils import plot_and_save_history
+
+
+def main():
+
+    random_state = 42
+    np.random.seed(random_state)
+
+    data_dir_path = './data'
+    model_dir_path = './models'
+    report_dir_path = './reports'
+
+    url_data = load_url_data(data_dir_path)
+
+    text_model = extract_text_model(url_data['text'])
+
+    batch_size = 64
+    epochs = 50
+
+    classifier = CnnLstmPredictor()
+
+    history = classifier.fit(text_model=text_model,
+                             model_dir_path=model_dir_path,
+                             url_data=url_data, batch_size=batch_size, epochs=epochs)
+
+    plot_and_save_history(history, CnnLstmPredictor.model_name,
+                          report_dir_path + '/' + CnnLstmPredictor.model_name + '-history.png')
+
+
+if __name__ == '__main__':
+    main()
